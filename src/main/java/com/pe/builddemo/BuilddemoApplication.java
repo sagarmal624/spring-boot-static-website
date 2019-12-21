8f5034459b1dc404eb1342f1df9a16e0835e2593
Sagar Shankhala <sagshank@publicisgroupe.net>
2019-12-21 13:38:58 +0530

    added new files'

---
+++ src/main/java/com/pe/builddemo/BuilddemoApplication.java
@@ -0,0 +1,13 @@
+package com.pe.builddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuilddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuilddemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pe/builddemo/BuilddemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pe.builddemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BuilddemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
