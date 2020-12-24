e670a6d14963c91e3c85c30cb0b32e628b9f2d03
Sagar Shankhala <sagshank@publicisgroupe.net>
2020-12-24 06:48:44 +0530

    fixed issue of port

--- src/main/java/com/pe/builddemo/BuilddemoApplication.java
+++ src/main/java/com/pe/builddemo/BuilddemoApplication.java
@@ -2,9 +2,15 @@ package com.pe.builddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BuilddemoApplication {
+public class BuilddemoApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BuilddemoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BuilddemoApplication.class, args);
